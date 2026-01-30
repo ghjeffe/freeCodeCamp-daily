89a01ebb5424853ce6cc93b938c40027026b14c4
ghjeffe <garyjeffersii@gmail.com>
2026-01-29 21:11:56 -0600

    Functioning local version

--- leap year calculator/main.py
+++ leap year calculator/main.py
@@ -1,13 +1,35 @@
 def is_leap_year(year):
-    if year % 400 != 0:
+    if year % 400 == 0: #all years divisable by 400 are leap years
+        return True
+    elif year % 100 == 0: #years not divisable by 400, but divisable by 100 are NOT leap years
         return False
-    if year % 100 != 0:
+    elif year % 4 == 0: #what's left is a leap year if divisable by 4
+        return True
+    else: #what's left is not a leap year
         return False
-        
-    if year % 4 != 0:
-        return False
-        
-    else:
-        return True 
-for num in [2004,6767,2026,2100,1999]:
+
+for num in sorted(set([
+    2004
+    ,6767
+    ,2026
+    ,2100
+    ,1999
+    ,1999
+    ,1999
+    ,1999
+    ,6767
+    ,6768
+    ,2028
+    ,2103
+    ,2003
+    ,2004
+    ,2005
+    ,2006
+    ,2034
+    ,2109
+    ,2009
+    ,2010
+    ,2011
+    ,2012
+])):
     print((f'{num}:{is_leap_year(num)}'))
